09657fc6008aa5a8b5ed229f2baf5a87386301de
Pierre Goiffon <pierre.goiffon@combodo.com>
2019-12-24 16:09:53 +0100

    AttributeString init

--- launch.php
+++ launch.php
@@ -16,3 +16,17 @@ echo "after child class init\n";
 echo "before child class init : lvl 3\n";
 $oChildClass = new Child_1_1_1('test', array());
 echo "after child class init\n";
+
+
+
+require_once 'attributedef.class.inc.php';
+echo "before new AttributeString\n";
+$oAttDef = new AttributeString('test', array(
+		'allowed_values' => array(1,2),
+		'depends_on' => null,
+		'sql' => 'test',
+		'default_value' => '',
+		'is_null_allowed' => true,
+	)
+);
+echo "after new AttributeString\n";
